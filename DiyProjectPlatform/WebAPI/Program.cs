d0583e4fe0515c1c14596e98764aa709884e2e5b
mihaelsegulja <mihael.segulja@gmail.com>
2025-04-08 11:24:37 +0200

    Add Models with ef scaffolding

--- DiyProjectPlatform/WebAPI/Program.cs
+++ DiyProjectPlatform/WebAPI/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DbDiyProjectPlatformContext>(options => {
+    options.UseSqlServer("name=ConnectionStrings:Exercise4ConnStr");
+});
 
 var app = builder.Build();
 
