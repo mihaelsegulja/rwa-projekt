a9c9798f22dc2304ddbc73184261bcbb282725f6
mihaelsegulja <mihael.segulja@gmail.com>
2025-04-10 19:21:09 +0200

    Add Controllers templates

---
+++ DiyProjectPlatform/WebAPI/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    private readonly DbDiyProjectPlatformContext _context;
+    
+    public UserController(DbDiyProjectPlatformContext context)
+    {
+        _context = context;
+    }
+}
