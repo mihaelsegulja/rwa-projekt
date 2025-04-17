b4c1c664d093940f7131afcd08703744ddece212
mihaelsegulja <mihael.segulja@gmail.com>
2025-04-17 13:49:53 +0200

    Change namespace to file-scoped

--- DiyProjectPlatform/WebAPI/Config/JwtTokenConfig.cs
+++ DiyProjectPlatform/WebAPI/Config/JwtTokenConfig.cs
@@ -1,10 +1,9 @@
-﻿namespace WebAPI.Config
+﻿namespace WebAPI.Config;
+
+public static class JWTTokenConfig
 {
-    public static class JWTTokenConfig
-    {
-        public static string TokenSecret { get; set; }
-        public static string TokenIssuer { get; set; }
-        public static string TokenAudience { get; set; }
-        public static int TokenExpiration { get; set; }
-    }
+    public static string TokenSecret { get; set; }
+    public static string TokenIssuer { get; set; }
+    public static string TokenAudience { get; set; }
+    public static int TokenExpiration { get; set; }
 }
